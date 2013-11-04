9b8ef3bf51a35479709aafcd859231f7fdf4a562
guangyao <cgy8888@gmail.com>
2013-11-05 06:36:39 +0800

    fix bugs

--- bilibilitv/myapp/utils.py
+++ bilibilitv/myapp/utils.py
@@ -1,3 +1,4 @@
+# coding=UTF-8
 from urlparse import urlparse
 from lxml import etree
 from lxml.html import fromstring
@@ -7,7 +8,10 @@ from common import view_data
 import sys
 import urllib2
 import os
+import time
 
+STATIC_ROOT = os.path.dirname(__file__)
+DOWNLOAD_ROOT = os.path.join(STATIC_ROOT,'downloads')
 
 def get_aid(url):
     o = urlparse(url)
@@ -23,14 +27,18 @@ def get_aid(url):
     
 
 def get_video_source(cid):
-    url = 'http://interface.bilibili.tv/playurl?otype=json&cid=%s' % (cid)
+    url = 'http://interface.bilibili.tv/playurl?cid=%s' % (cid)
     
+    print '@32',url
+
     res = requests.get(url)
     content = res.content
-    if res.headers['content-type'].find('text/xml')>=0:
-        doc = fromstring(content)
-        result = doc.xpath('//result')[0].text_content()
-        return {'result':result}
+
+    parser = etree.XMLParser(strip_cdata=False)
+    doc = etree.fromstring(content,parser)
+    result = doc.xpath('//result')[0].text_content()
+    print '@37',etree.tostring(doc)
+    
     return simplejson.loads(content)
 
 def get_comment_source(cid):
@@ -54,15 +62,17 @@ def get_comment_source(cid):
     
     return data_dict
 
-def download_file(url):
+def download_file(url,sub_dir=''):
     file_name = url.split('/')[-1]
     if file_name.find('?')>=0:
 #        fix for sina video
         file_name = file_name.split('?')[0]
 
-    if os.path.isfile(file_name):
-        print '@58,',os.path.isfile(file_name)
-        return file_name
+    if sub_dir:
+        file_name = os.path.join(DOWNLOAD_ROOT,'%s/%s'% (sub_dir,file_name))
+    else:
+        file_name = os.path.join(DOWNLOAD_ROOT,file_name)
+
     u = urllib2.urlopen(url)
     f = open(file_name, 'wb')
     meta = u.info()
@@ -83,16 +93,69 @@ def download_file(url):
         print status,
 
     f.close()
+
+#    check file download complete
+    download_size = os.path.getsize(file_name)
+    if file_size != download_size:
+        download_file(url,sub_dir=sub_dir)
+
     return file_name
 
 def convert_flv_to_mp4(old_file,new_file):
-    os.system('ffmpeg -i "%s" "%s" 2> /dev/null' % (old_file,new_file))
-    
+    os.system('ffmpeg -i "%s" "%s"' % (old_file,new_file))
     return new_file
+
+def merge_files(file_list,new_file):
+    fout = file(new_file,'wb')
+
+    for n in file_list:
+        fin = file(n,'rb')
+        while True:
+            data = fin.read(65536)
+            if not data:
+                break
+            fout.write(data)
+        fin.close()
+    fout.close()
+
+def sort_list_by_order(li):
+    new_list = sorted(li,key=lambda k:k['order'])
+    return new_list
+
+def get_video(source_json,video_title=''):
+    if source_json['result']!='error':
+
+        print '@121',source_json
+        print '@122',video_title
+
+        url_list= source_json['durl']
+        url_list = sort_list_by_order(url_list)
+        file_list = []
+        list_txt = os.path.join(DOWNLOAD_ROOT,'%s/list.txt' % (video_title))
+
+        if not os.path.exists(os.path.dirname(list_txt)):
+            os.makedirs(os.path.dirname(list_txt))
+            
+        f = open(list_txt,'wb')
+        for d in url_list:
+            source_url = d['url']
+            file_name = download_file(source_url,video_title)
+            file_name = file_name.split('/')[-1]
+            file_list.append(file_name)
+            
+            f.write("file '%s'\n" % (file_name))
+
+        f.close()
+        print '@146,download ok.'
+
+        mp4= os.path.join(DOWNLOAD_ROOT,'%s/%s-%s.mp4'%(video_title,aid,cid))
+        command = u'ffmpeg -f concat -i %s -c copy %s' % (list_txt,mp4)
+        command = command.encode('utf8')
+        os.system(command)
+
 if __name__ == '__main__':
 #    download the video and megre it to mp4 file then upload to oss
     bilibili_url = sys.argv[1]
-
     aid = get_aid(bilibili_url)
     
     if not aid:
@@ -100,14 +163,16 @@ if __name__ == '__main__':
         sys.exit()
     data_dict = view_data(aid)
 
-    cid,pages = data_dict['cid'],data_dict['pages']
+    cid,pages = data_dict['cid'],int(data_dict['pages'])
 
-    source_json = get_video_source(cid)
-    if source_json['result']!='error':
-        url_list= source_json['durl']
-        print '@104,',url_list
-        for d in url_list:
-            source_url = d['url']
-            file_name = download_file(source_url)
-            new_file = file_name + '.mp4'
-            convert_flv_to_mp4(file_name,new_file)
+#    source_json = get_video_source(cid)
+    
+    for i in range(1,pages+1):
+        print '@133,read for get video at page %d' % i
+        time.sleep(1)
+        data_dict = view_data(aid,i)
+        cid = data_dict['cid']
+        source_json = get_video_source(cid)
+        get_video(source_json,data_dict['partname'])
+
+
